45c7ab2b2f83fb1e7a7697db4955a07841e6f047
Richard Wilborn <45742979+richardwilborn@users.noreply.github.com>
2022-11-07 12:46:13 -0600

    added routes and server.js file

---
+++ routes/apiRoutes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const store = require('../db/store');
+
+router.get('/notes', (req, res) => {
+  store
+    .getNotes()
+    .then((notes) => {
+      return res.json(notes);
+    })
+    .catch((err) => res.status(500).json(err));
+});
+
+router.post('/notes', (req, res) => {
+  store
+    .addNote(req.body)
+    .then((note) => res.json(note))
+    .catch((err) => res.status(500).json(err));
+});
+
+module.exports = router;
