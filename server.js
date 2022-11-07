f6e7d9e49e64fa0bdbd7d8e213e1c39271b8ea4e
Richard Wilborn <45742979+richardwilborn@users.noreply.github.com>
2022-11-07 14:36:46 -0600

    added require routes to server.js

--- server.js
+++ server.js
@@ -1,1 +1,6 @@
 const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
