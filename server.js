7806020afe40a09dce1edd03eeb4b42064bfdb27
Richard Wilborn <45742979+richardwilborn@users.noreply.github.com>
2022-11-07 14:40:53 -0600

    added parsing, static and route middleware. added app.listen

--- server.js
+++ server.js
@@ -4,3 +4,11 @@ const htmlRoutes = require('./routes/htmlRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
