8e2d25e4f9fcdc4d23a697103596fafcfca9627f
Richard Wilborn <45742979+richardwilborn@users.noreply.github.com>
2022-11-22 13:11:19 -0600

    added code to store.js file

--- db/store.js
+++ db/store.js
@@ -3,3 +3,21 @@ const fs = require('fs');
 
 const uuidv1 = require('uuidv1');
 
+class Store {
+  read() {
+    return fs.readFileAsync('db/db.json', 'utf8');
+  }
+
+  write(note) {
+    return writeFileAsync('db/db.json', JSON.stringify(note));
+}
+
+addNote(note) {
+  const { title, text } = note;
+  if (!title || !text) {
+    throw new Error("Note title and text cannot be empty");
+
+  const newNote = { title, text id: uuidv1() };
+
+  }
+}
