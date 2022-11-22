ba081f78bcc4533eb2c8b1d95060e1125bc6c1de
Richard Wilborn <45742979+richardwilborn@users.noreply.github.com>
2022-11-22 13:26:25 -0600

    finished code in store.js

--- db/store.js
+++ db/store.js
@@ -1,7 +1,9 @@
 const util = require('util');
 const fs = require('fs'); 
 
-const uuidv1 = require('uuidv1');
+const uuidv1 = require('uuid/v1');
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 class Store {
   read() {
@@ -12,26 +14,30 @@ class Store {
     return writeFileAsync('db/db.json', JSON.stringify(note));
 }
 
-getNotes() {
-  return this.read().then((notes) => {
-    let parsedNotes;
-    try {
-      parsedNotes = JSON.parse(notes);
-    } catch (err) {
-      parsedNotes = [];
-    }
-    
-    return parsedNotes;
-  });
-}
+  getNotes() {
+    return this.read().then((notes) => {
+      let parsedNotes;
+      try {
+        parsedNotes = [].concat(JSON.parse(notes));
+      } catch (err) {
+        parsedNotes = [];
+      }
+      
+      return parsedNotes;
+    });
+  }
 
 addNote(note) {
   const { title, text } = note;
   if (!title || !text) {
-    throw new Error("Note title and text cannot be empty");
-
-  const newNote = { title, text id: uuidv1() };
-
+    throw new Error("Note 'title' and 'text' cannot be empty");
+  }
+  const newNote = { title, text, id: uuidv1() };
+  
+  return this.getNotes()
+    .then((notes) => [...notes, newNote])
+    .then((updatedNotes) => this.write(updatedNotes))
+    .then(() => newNote);
   }
 }
 
